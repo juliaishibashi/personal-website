81ef6ceaf93e3e2071211eef334e2b1c8eb59216
Julia Ishibashi <juliaishibashi@uvic.ca>
2024-05-28 18:58:35 -0700

    fix the comment

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,14 +14,10 @@ const About = () => {
         </div>
         <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
           <div className='sm:text-right text-5xl font-bold text-[#869F77]'>
-            <p>Hello, My name is Julia! Please take a look around</p>
+            <p>Hi there! I am Julia!</p>
           </div>
           <div>
-            <p className='text-[#588061]'>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>
+            <p className='text-[#588061]'>I was raised in Japan and now I am a third-year Computer Science student at UVic. My favorite subject is algorithms because is not it amazing how the concept of a tree was invented?! Although I have not done much coding outside of class assignments before, I recently started working on my own projects. I have got plenty of ideas, so stay tuned for more updates! </p>
           </div>
         </div>
       </div>
