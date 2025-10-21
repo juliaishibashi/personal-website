e0b2a8ba018f1c9f106ddf4b195f98922db02513
Julia Ishibashi <juliaishibashi@uvic.ca>
2025-10-21 11:32:38 -0700

    commentout app.js

--- src/App.js
+++ src/App.js
@@ -51,39 +51,39 @@
 // };
 // export default App;
 
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-// import About from './components/About';
-import Skills from './components/Skills';
-import Works from './components/Works';
-import ProjectDetail from './components/ProjectDetail';
-import Contact from './components/Contact';
-import Profile from './components/Profile';
+// import React from 'react';
+// import { Routes, Route, useLocation } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Home from './components/Home';
+// // import About from './components/About';
+// import Skills from './components/Skills';
+// import Works from './components/Works';
+// import ProjectDetail from './components/ProjectDetail';
+// import Contact from './components/Contact';
+// import Profile from './components/Profile';
 
-const App = () => {
-  const location = useLocation();
-  const hideNavbarPaths = ['/profile'];
+// const App = () => {
+//   const location = useLocation();
+//   const hideNavbarPaths = ['/profile'];
 
-  return (
-    <>
-      {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
-      <Routes>
-        <Route path="/" element={
-          <>
-            <div id="home"><Home /></div>
-            {/* <div id="about"><About /></div> */}
-            <div id="skills"><Skills /></div>
-            <div id="works"><Works /></div>
-            <div id="contact"><Contact /></div>
-          </>
-        } />
-        <Route path="/projects/:id" element={<ProjectDetail />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </>
-  );
-};
+//   return (
+//     <>
+//       {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
+//       <Routes>
+//         <Route path="/" element={
+//           <>
+//             <div id="home"><Home /></div>
+//             {/* <div id="about"><About /></div> */}
+//             <div id="skills"><Skills /></div>
+//             <div id="works"><Works /></div>
+//             <div id="contact"><Contact /></div>
+//           </>
+//         } />
+//         <Route path="/projects/:id" element={<ProjectDetail />} />
+//         <Route path="/profile" element={<Profile />} />
+//       </Routes>
+//     </>
+//   );
+// };
 
-export default App;
+// export default App;
